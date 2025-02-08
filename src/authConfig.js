517692a8809c3adebf78f849bc5e75aefab01311
SophiaSHuangMck <sophia_s_huang@external.mckinsey.com.cn>
2025-02-08 14:37:13 +0800

    changeRedirect

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "832f193a-f669-4080-ae1b-f6e43e9de065",
       authority: "https://login.chinacloudapi.cn/ee02fc79-a0ad-44bc-b47d-03d0491fbe48",
-      redirectUri: "http://localhost:3000/auth/callback",
+      redirectUri: "https://ctetranswebdev.chinacloudsites.cn/auth/callback",
     },
     cache: {
       cacheLocation: "sessionStorage",
